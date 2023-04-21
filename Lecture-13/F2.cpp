8924258890fb3035d7881fc4317748fedfc23a66
AbhishekYadav2022 <106291898+AbhishekYadav2022@users.noreply.github.com>
2023-04-21 18:55:54 +0530

    lec13

--- Lecture-13/F2.cpp
+++ Lecture-13/F2.cpp
@@ -1,10 +1,46 @@
-// Peak Index in a Mountain Array
-// Problem: https://leetcode.com/problems/peak-index-in-a-mountain-array/
-
+// Question: https://leetcode.com/problems/peak-index-in-a-mountain-array/
+// Peak Index In a Mountain Array
 #include <iostream>
 using namespace std;
+
+int peakIndexInMountainArray(int arr[], int size)
+{
+    int s = 0;
+    int e = size - 1;
+
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        {
+            if (arr[mid] < arr[mid + 1])
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid;
+            }
+            mid = s + (e - s) / 2;
+        }
+    }
+    return s;
+}
+
 int main()
 {
-    
+    int arr[11] = {1, 2, 3, 7, 9, 14, 54, 44, 31, 20, 9};
+
+    cout << "Peak index: " << peakIndexInMountainArray(arr, 13) << endl;
     return 0;
 }
+
+/*
+Finding index of peak element in an array of integers
+             15
+           11/\11
+           9/  \7
+          7/    \5
+         4/      \3
+        2/
+*/
